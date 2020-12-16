b0518875baba24f3854fcd4e98b66bb3508885a6
at99nd-progammer <at99nd@gmail.com>
2020-12-16 11:20:04 +0700

    Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      params[:session][:remember_me] == Settings.number.checkbox ? remember(user) : forget(user)
       flash[:success] = t ".message_success_login"
       redirect_to user
     else
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = t ".message_success_logout"
     redirect_to home_path
   end
