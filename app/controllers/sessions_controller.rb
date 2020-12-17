535f8b998fc0e295503a0e040975b55f1f75a282
at99nd-progammer <at99nd@gmail.com>
2020-12-18 00:54:54 +0700

    Updating,Dhowing,Deleting Users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == Settings.number.checkbox ? remember(user) : forget(user)
       flash[:success] = t ".message_success_login"
-      redirect_to user
+      redirect_back_or user
     else
       flash[:danger] = t ".message"
       render :new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_012045) do
+ActiveRecord::Schema.define(version: 2020_12_17_174453) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_12_16_012045) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
